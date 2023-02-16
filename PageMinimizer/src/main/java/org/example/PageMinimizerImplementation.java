44922ca69e0a4d0830b904b9ec6cf8ba5fcd981c
dvvvelop <dvvvelop@mail.ru>
2023-02-16 19:09:52 +0300

    commit3

--- PageMinimizer/src/main/java/org/example/PageMinimizerImplementation.java
+++ PageMinimizer/src/main/java/org/example/PageMinimizerImplementation.java
@@ -8,27 +8,53 @@ public class PageMinimizerImplementation implements PageMinimizer {
     @Override
     public String string1tostring2(String n1) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите номера страниц через запятую: ");
-        String str = in.nextLine();
-        //System.out.println(str); //задаем строку чисел
 
+        Scanner scanner = new Scanner(System.in);
 
-        String strArr[] = str.split(",");
-        int numArr[] = new int[strArr.length];
-        for (int i = 0; i < strArr.length; i++) {
-            numArr[i] = Integer.parseInt(strArr[i]);
-            //System.out.println(numArr[i]); //преобразуем ее в массив
+        // Read a string of numbers from the console
+        System.out.print("Enter a string of numbers: ");
+        String numberString = scanner.nextLine();
+
+        // Convert the string to an array of integers
+        String[] numberStrings = numberString.split("\\s*,\\s*");
+        int[] numbers = new int[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            numbers[i] = Integer.parseInt(numberStrings[i]);
         }
 
-        Arrays.sort(numArr);
+        // Sort the array in ascending order
+        Arrays.sort(numbers);
 
-        for (int values : numArr) {
-            System.out.print(values + ", ");
-
+        // Add a dash between more than two consecutive numbers, and a comma between other numbers
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (i < numbers.length - 1 && numbers[i + 1] == numbers[i] + 1) {
+                count++;
+            } else {
+                if (count > 2) {
+                    sb.append(numbers[i - count + 1]).append("-").append(numbers[i]).append(",");
+                } else if (count == 2) {
+                    sb.append(numbers[i - 1]).append(",").append(numbers[i]).append(",");
+                } else {
+                    sb.append(numbers[i]).append(",");
+                }
+                count = 1;
+            }
         }
 
-
+        // Print the final string with dashes and commas
+        String result = sb.toString();
+        if (result.endsWith(",")) {
+            result = result.substring(0, result.length() - 1);
+        }
+        System.out.println(result);
         return "";
     }
-}
+        }
+
+
+
+
+
+
